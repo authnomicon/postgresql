a0e3cbbd7f6419a1b63dc7fe5a3ba90410e11194
Jared Hanson <jaredhanson@gmail.com>
2024-04-29 14:25:22 -0700

    Add tests.

---
+++ test/package.test.js
@@ -0,0 +1,23 @@
+/* global describe, it */
+
+var expect = require('chai').expect;
+
+
+describe('@authnomicon/sqlite', function() {
+  
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('org.authnomicon/postgresql');
+      expect(json.assembly.components).to.deep.equal([
+        'directory',
+        'credentials/federatedidstore',
+        'credentials/passwordstore',
+        'oauth/clientdirectory',
+        'oauth/credentials/clientsecretstore'
+      ]);
+    });
+  });
+  
+});
