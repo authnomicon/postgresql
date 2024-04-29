c8beb27c1b98728b038a3c7658554046eecdd845
Jared Hanson <jaredhanson@gmail.com>
2024-04-29 14:13:08 -0700

    Expand user schema.

--- lib/schema/users.sql
+++ lib/schema/users.sql
@@ -1,3 +1,9 @@
+CREATE TYPE url AS (
+  location TEXT,
+  type TEXT,
+  is_primary BOOLEAN
+);
+
 CREATE TYPE email AS (
   address TEXT,
   type TEXT,
@@ -5,7 +11,26 @@ CREATE TYPE email AS (
   is_verified BOOLEAN
 );
 
+CREATE TYPE phone_number AS (
+  number TEXT,
+  type TEXT,
+  is_primary BOOLEAN,
+  is_verified BOOLEAN
+);
+
+CREATE TYPE address AS (
+  street_address TEXT,
+  locality TEXT,
+  region TEXT,
+  postal_code TEXT,
+  country TEXT,
+  type TEXT,
+  is_primary BOOLEAN
+);
+
 -- postgres won't allow a singulear "user" table, so violating that convention here
+-- TODO: create a trigger to update updated_at
+-- TODO: zoneinfo/utcoffset and locale
 CREATE TABLE users (
   user_id TEXT PRIMARY KEY,
   username TEXT UNIQUE,
@@ -15,5 +40,14 @@ CREATE TABLE users (
   middle_name TEXT,
   honorific_prefix TEXT,
   honorific_suffix TEXT,
-  emails email[]
+  nickname TEXT,
+  photos url[],
+  urls url[],
+  emails email[],
+  phone_numbers phone_number[],
+  gender TEXT,
+  birthday DATE,
+  addresses address[],
+  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
