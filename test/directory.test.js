97e651fb17f7517f9613504248292235a4993e08
Jared Hanson <jaredhanson@gmail.com>
2024-05-02 13:42:13 -0700

    Fix tests.

--- test/directory.test.js
+++ test/directory.test.js
@@ -14,7 +14,9 @@ describe('directory', function() {
     
     it('should map family name and given name', function(done) {
       var client = new Object();
-      client.query = sinon.stub().yieldsAsync(null, {
+      client.query = sinon.stub();
+      client.query.onFirstCall().resolves(null);
+      client.query.onSecondCall().yieldsAsync(null, {
         rows: [
           {
             user_id: '703887',
@@ -23,29 +25,36 @@ describe('directory', function() {
           }
         ]
       });
+      
+      var postgres = new Object();
+      postgres.createConnectionPool = sinon.stub().returns(client);
     
-    
-      var directory = factory(client);
-      directory.read('703887', function(err, user) {
-        if (err) { return done(err); }
+      var directory = factory('postgresql://www.example.com/exampledb', postgres)
+        .then(function(directory) {
+          expect(postgres.createConnectionPool).to.have.been.calledOnceWith('postgresql://www.example.com/exampledb');
+          
+          directory.read('703887', function(err, user) {
+            if (err) { return done(err); }
         
-        expect(client.query).to.have.been.calledOnce;
-        var sql = client.query.getCall(0).args[0];
-        var values = client.query.getCall(0).args[1];
-        expect(sql).to.equal('SELECT *    FROM users   WHERE user_id = $1');
-        expect(values).to.deep.equal([ '703887' ]);
+            expect(client.query).to.have.been.calledTwice;
+            var sql = client.query.getCall(1).args[0];
+            var values = client.query.getCall(1).args[1];
+            expect(sql).to.equal('SELECT *    FROM users   WHERE user_id = $1');
+            expect(values).to.deep.equal([ '703887' ]);
         
-        expect(user).to.deep.equal({
-          id: '703887',
-          name: {
-            familyName: 'Hashimoto',
-            givenName: 'Mork'
-          }
-        });
-        done();
-      });
-    });
+            expect(user).to.deep.equal({
+              id: '703887',
+              name: {
+                familyName: 'Hashimoto',
+                givenName: 'Mork'
+              }
+            });
+            done();
+          });
+        })
+        .catch(done);
+    }); // should map family name and given name
     
-  });
+  }); // #read
   
 });
