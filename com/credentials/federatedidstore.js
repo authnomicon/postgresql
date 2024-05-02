d28f278f5e78bfb4832c708369b8247e8ba1c20b
Jared Hanson <jaredhanson@gmail.com>
2024-05-02 11:20:19 -0700

    Connect to pg via bixby-postgresql.

--- com/credentials/federatedidstore.js
+++ com/credentials/federatedidstore.js
@@ -1,13 +1,29 @@
 var FederatedIDStore = require('../../lib/credentials/federatedidstore');
 
-exports = module.exports = function(db) {
-  //console.log('CONNECT TO POSTGRES!');
+exports = module.exports = function($uri, postgresql) {
+  var pool = postgresql.createConnectionPool($uri);
   
-  return new FederatedIDStore(db);
+  return pool.query('SELECT to_regclass($1::text)', [ 'federated_credential' ])
+    .then(function(res) {
+      if (res && res.rows && res.rows[0] && res.rows[0]['to_regclass'] === null) {
+        // TODO: log this with logger
+        console.log('creating users table in: ');
+      
+        var sql = fs.readFileSync(path.join(__dirname, '../../lib/schema/federated_credential.sql'), 'utf8');
+        return pool.query(sql);
+      }
+    })
+    .then(function() {
+      return new FederatedIDStore(pool);
+    });
 };
 
 exports['@singleton'] = true;
 exports['@implements'] = 'module:@authnomicon/credentials.FederatedIDStore';
+exports['@service'] = 'apcred-postgresql';
+exports['@port'] = 5432;
+exports['@env'] = [ 'DATABASE_URL' ];
 exports['@require'] = [
-  '../database'
+  '$uri',
+  'module:bixby-postgresql'
 ];
--- com/oauth/credentials/clientsecretstore.js
+++ com/oauth/credentials/clientsecretstore.js
@@ -1,13 +1,30 @@
 var ClientSecretStore = require('../../../lib/oauth/credentials/clientsecretstore');
 
-exports = module.exports = function(db) {
+exports = module.exports = function($uri, postgresql) {
   //console.log('CONNECT TO POSTGRES!');
+  var pool = postgresql.createConnectionPool($uri);
   
-  return new ClientSecretStore(db);
+  return pool.query('SELECT to_regclass($1::text)', [ 'client' ])
+    .then(function(res) {
+      if (res && res.rows && res.rows[0] && res.rows[0]['to_regclass'] === null) {
+        // TODO: log this with logger
+        console.log('creating users table in: ');
+  
+        var sql = fs.readFileSync(path.join(__dirname, '../../lib/schema/client.sql'), 'utf8');
+        return pool.query(sql);
+      }
+    })
+    .then(function() {
+      return new ClientSecretStore(pool);
+    });
 };
 
 exports['@singleton'] = true;
 exports['@implements'] = 'http://i.authnomicon.org/oauth2/ClientSecretService';
+exports['@service'] = 'apclient-postgresql';
+exports['@port'] = 5432;
+exports['@env'] = [ 'DATABASE_URL' ];
 exports['@require'] = [
-  '../../database'
+  '$uri',
+  'module:bixby-postgresql'
 ];
