1444bbc942a6230bc028bcd192097db8a62968f2
Jared Hanson <jaredhanson@gmail.com>
2024-05-13 14:16:18 -0700

    Add tests.

--- test/credentials/federatedidstore.test.js
+++ test/credentials/federatedidstore.test.js
@@ -12,7 +12,7 @@ describe('credentials/federatedidstore', function() {
   
   describe('#find', function() {
     
-    it('should find credential', function(done) {
+    it('should find user with credential', function(done) {
       var client = new Object();
       client.query = sinon.stub();
       client.query.onFirstCall().resolves(null);
@@ -55,7 +55,44 @@ describe('credentials/federatedidstore', function() {
           });
         })
         .catch(done);
-    }); // should find credential
+    }); // should find user with credential
+    
+    it('should not find user with non-existent credential', function(done) {
+      var client = new Object();
+      client.query = sinon.stub();
+      client.query.onFirstCall().resolves(null);
+      client.query.onSecondCall().yieldsAsync(null, {
+        rows: []
+      });
+      
+      var postgres = new Object();
+      postgres.createConnectionPool = sinon.stub().returns(client);
+      
+      var store = factory('postgresql://www.example.com/exampledb', postgres)
+        .then(function(store) {
+          expect(postgres.createConnectionPool).to.have.been.calledOnceWith('postgresql://www.example.com/exampledb');
+          
+          var subject = {
+            id: '248289761001',
+            displayName: 'Jane Doe'
+          }
+          var provider = 'https://server.example.com';
+          
+          store.find(subject, provider, function(err, user) {
+            if (err) { return done(err); }
+            
+            expect(client.query).to.have.been.calledTwice;
+            var sql = client.query.getCall(1).args[0];
+            var values = client.query.getCall(1).args[1];
+            expect(sql).to.equal('SELECT *    FROM federated_credentials   WHERE provider = $1 AND subject = $2');
+            expect(values).to.deep.equal([ 'https://server.example.com', '248289761001' ]);
+            
+            expect(user).to.be.undefined;
+            done();
+          });
+        })
+        .catch(done);
+    }); // should not find user with non-existent credential
     
   }); // #find
   
