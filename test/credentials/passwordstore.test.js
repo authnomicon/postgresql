15bd72e42efbccfe7487b5b38892ee422f4b515d
Jared Hanson <jaredhanson@gmail.com>
2024-05-14 11:06:23 -0700

    Add tests.

--- test/credentials/passwordstore.test.js
+++ test/credentials/passwordstore.test.js
@@ -62,4 +62,47 @@ describe('credentials/passwordstore', function() {
     
   }); // #create
   
+  describe('#verify', function() {
+    
+    it('should verify password', function(done) {
+      var client = new Object();
+      client.query = sinon.stub();
+      client.query.onFirstCall().resolves(null);
+      client.query.onSecondCall().yieldsAsync(null, {
+        rows: [
+          {
+            user_id: '703887',
+            username: 'mhashimoto',
+            hashed_password: '$pbkdf2-sha256$i=310000$43I1LSSycXBMyfCHL81CKA$urMNIvsWf8bNmVP263MybiMPN0JV/UWXyaTMPJWMsrs'
+          }
+        ]
+      });
+      
+      var postgres = new Object();
+      postgres.createConnectionPool = sinon.stub().returns(client);
+      
+      var store = factory('postgresql://www.example.com/exampledb', postgres)
+        .then(function(store) {
+          expect(postgres.createConnectionPool).to.have.been.calledOnceWith('postgresql://www.example.com/exampledb');
+        
+          store.verify('mhashimoto', 'letmein', function(err, user) {
+            if (err) { return done(err); }
+            
+            expect(client.query).to.have.been.calledTwice;
+            var sql = client.query.getCall(1).args[0];
+            var values = client.query.getCall(1).args[1];
+            expect(sql).to.equal('SELECT *    FROM users   WHERE username = $1');
+            
+            expect(user).to.deep.equal({
+              id: '703887',
+              username: 'mhashimoto'
+            });
+            done();
+          });
+        })
+        .catch(done);
+    }); // should verify password
+    
+  }); // #verify
+  
 });
