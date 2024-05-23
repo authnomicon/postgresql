cca057297896f8974199a817afcae20de8028447
Jared Hanson <jaredhanson@gmail.com>
2024-05-23 14:47:54 -0700

    Add tests.

--- test/directory.test.js
+++ test/directory.test.js
@@ -55,6 +55,47 @@ describe('directory', function() {
         .catch(done);
     }); // should map family name and given name
     
+    it('should map given name', function(done) {
+      var client = new Object();
+      client.query = sinon.stub();
+      client.query.onFirstCall().resolves(null);
+      client.query.onSecondCall().yieldsAsync(null, {
+        rows: [
+          {
+            user_id: '703887',
+            given_name: 'Mork'
+          }
+        ]
+      });
+      
+      var postgres = new Object();
+      postgres.createConnectionPool = sinon.stub().returns(client);
+    
+      var directory = factory('postgresql://www.example.com/exampledb', postgres)
+        .then(function(directory) {
+          expect(postgres.createConnectionPool).to.have.been.calledOnceWith('postgresql://www.example.com/exampledb');
+          
+          directory.read('703887', function(err, user) {
+            if (err) { return done(err); }
+        
+            expect(client.query).to.have.been.calledTwice;
+            var sql = client.query.getCall(1).args[0];
+            var values = client.query.getCall(1).args[1];
+            expect(sql).to.equal('SELECT *    FROM users   WHERE user_id = $1');
+            expect(values).to.deep.equal([ '703887' ]);
+        
+            expect(user).to.deep.equal({
+              id: '703887',
+              name: {
+                givenName: 'Mork'
+              }
+            });
+            done();
+          });
+        })
+        .catch(done);
+    }); // should map given name
+    
     it('should map emails with address', function(done) {
       var client = new Object();
       client.query = sinon.stub();
