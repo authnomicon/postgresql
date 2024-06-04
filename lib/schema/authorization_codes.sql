884f779908f6cc31a24fcf421f6578eb9f4a3395
Jared Hanson <jaredhanson@gmail.com>
2024-06-04 14:51:55 -0700

    Store hashed authorization code.

--- lib/schema/authorization_codes.sql
+++ lib/schema/authorization_codes.sql
@@ -1,9 +1,9 @@
 CREATE TABLE authorization_codes (
+  code_hash TEXT PRIMARY KEY,
   issuer TEXT NOT NULL,
   client_id TEXT NOT NULL,
   redirect_uri TEXT,
   user_id TEXT NOT NULL,
   scope TEXT[],
-  session_id TEXT,
-  code TEXT UNIQUE NOT NULL
+  session_id TEXT
 );
