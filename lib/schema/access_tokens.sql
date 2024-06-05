fb453ee943a6f37a06f76cc6cd5b5ff99036abfd
Jared Hanson <jaredhanson@gmail.com>
2024-06-04 17:02:48 -0700

    Expand access token schema.

--- lib/schema/access_tokens.sql
+++ lib/schema/access_tokens.sql
@@ -1,6 +1,11 @@
 CREATE TABLE access_tokens (
   token_hash BYTEA PRIMARY KEY,
-  user_id TEXT NOT NULL,
-  client_id TEXT NOT NULL,
-  scope TEXT[]
+  token_id TEXT UNIQUE DEFAULT gen_random_uuid(),
+  user_id TEXT,
+  client_id TEXT,
+  audience TEXT[],
+  scope TEXT[],
+  issued_at TIMESTAMP DEFAULT now(),
+  valid_at TIMESTAMP DEFAULT now(),
+  expires_at TIMESTAMP DEFAULT now() + interval '1 day'
 );
--- lib/schema/authorization_codes.sql
+++ lib/schema/authorization_codes.sql
@@ -5,5 +5,7 @@ CREATE TABLE authorization_codes (
   redirect_uri TEXT,
   user_id TEXT NOT NULL,
   scope TEXT[],
-  session_id TEXT
+  session_id TEXT,
+  issued_at TIMESTAMP NOT NULL DEFAULT now(),
+  expires_at TIMESTAMP NOT NULL DEFAULT now() + interval '10 minutes'
 );
