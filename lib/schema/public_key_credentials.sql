1b9c960d03af443437b7b29f6a35c54f2d7d4c01
Jared Hanson <jaredhanson@gmail.com>
2024-07-05 16:52:35 -0700

    Add backup-related flags.

--- lib/schema/public_key_credentials.sql
+++ lib/schema/public_key_credentials.sql
@@ -3,5 +3,7 @@ CREATE TABLE public_key_credentials (
   public_key TEXT NOT NULL,
   sign_count INTEGER,
   transports TEXT[],
+  backup_eligible BOOLEAN,
+  backed_up BOOLEAN,
   user_id TEXT NOT NULL REFERENCES users(user_id)
 );
